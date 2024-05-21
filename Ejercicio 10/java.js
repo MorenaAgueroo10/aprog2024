f68b3dfb8c652e143104c823a3702416dedc678a
Morena Aguero <166777868+MorenaAgueroo10@users.noreply.github.com>
2024-05-21 00:47:21 -0300

    Add files via upload

---
+++ Ejercicio 10/java.js
@@ -0,0 +1,16 @@
+//Ejercicio 1
+const  nombre = "Morena";
+const apellido = "Agüero";
+
+console.log (nombre,apellido);
+
+//Ejercicio 2
+const num1 = 2;
+const num2 = 10;
+const suma = (num1 + num2);
+console.log(suma);
+
+//Ejercicio 3
+const num3 = suma + 8;
+console.log (num3);
+
